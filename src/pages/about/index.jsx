5cb6e67c3051f583f9f7e1443b74532d8737d508
Alex G. Mircean <56928436+bigmirc@users.noreply.github.com>
2022-10-05 21:24:30 +0300

    Initial styling (#3)
    * stiluri initiale
    
    * update

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,23 +1,15 @@
-import React from "react";
-import Head from "next/head";
+import React from 'react';
+import Head from 'next/head';
 
 const About = () => {
-	return (
-		<>
-			<Head>
-				<title>Cosminachio About</title>
-				<meta name="description" content="Need more details or you have a request for us? Drop us a line and we will get back to you as soon as possible." />
-				<meta property="og:title" content="Contact Us - Codalb" />
-				<meta property="og:description" content="Need more details or you have a request for us? Drop us a line and we will get back to you as soon as possible." />
-				<meta property="og:url" content="https://codalb.ro/contact" />
-				<meta property="og:image" content="/images/codalb_og.png" />
-				<meta property="og:image:secure_url" content="/images/codalb_og.png"/>
-				<meta property="og:type" content="website" />
-				<meta property="og:site_name" content="Codalb" />
-			</Head>
-			<h1>About ceva ceav</h1>
-		</>
-	);
-}
+  return (
+    <>
+      <Head>
+        <title>Cosminachio About</title>
+      </Head>
+      <h1>About ceva ceav</h1>
+    </>
+  );
+};
 
-export default About
+export default About;
---
+++ src/utils/projects.jsx
@@ -0,0 +1,11 @@
+const projects = [
+  'Impas',
+  'Castelul de Apă',
+  'Shapes',
+  'TypeStation',
+  'Eric Gill Short Book',
+  'Ambiant',
+  'Bucharest Buildings Pictograms',
+];
+
+export default projects;
