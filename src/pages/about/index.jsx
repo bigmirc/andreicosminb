d9449bb77270510783acd323e9c6bd89b7d94050
Alexandru Mircean <amircean@yahoo.com>
2022-10-06 23:52:08 +0300

    fix build issues

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import styles from './About.module.css';
-import cosminacho from '../../assets/cosminachou.jpeg'
+import Image from 'next/image';
 
 const About = () => {
   return (
@@ -10,20 +10,29 @@ const About = () => {
         <title>Cosminachio About</title>
       </Head>
       <div className={styles.container}>
-        <div className={styles.title}>
-          ABOUT
-        </div>
+        <div className={styles.title}>ABOUT</div>
         <div className={styles.wrapper}>
           <div className={styles.pwrapper}>
             <div className={styles.p}>
-            Hello! My name is Cosmin and I am a curious graphic designer based in Bucharest, Romania. I studied Graphic Design at National University of Arts in Bucharest and I currently work a full time job as a designer at Ivatherm. Before that I was a freelancer for a almost a year.
+              Hello! My name is Cosmin and I am a curious graphic designer based
+              in Bucharest, Romania. I studied Graphic Design at National
+              University of Arts in Bucharest and I currently work a full time
+              job as a designer at Ivatherm. Before that I was a freelancer for
+              a almost a year.
             </div>
             <div className={styles.p}>
-            I am interested in type design, branding, user interface design and editorial design.
+              I am interested in type design, branding, user interface design
+              and editorial design.
             </div>
           </div>
           <div className={styles.cosminacho}>
-            <img src={cosminacho.src} alt="" />
+            <Image
+              src={'/images/cosmin.jpeg'}
+              width="300px"
+              height="300px"
+              layout="responsive"
+              alt="cosminacho"
+            />
           </div>
         </div>
       </div>
