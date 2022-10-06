88fb48a36a9804f3fba6318a6a50aa857d99dc95
Alexandru Mircean <amircean@yahoo.com>
2022-10-07 00:09:22 +0300

    route fix

--- src/pages/work/[id].jsx
+++ src/pages/work/[id].jsx
@@ -1,19 +1,25 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import projects from '../../utils/projects';
 import styles from './work.module.css';
 
 const WorkDetail = () => {
   const router = useRouter();
   const { id } = router.query;
-  const project = projects[+id];
-  console.log(project);
+  const [project, setProject] = useState({});
+
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    setProject(projects[+id]);
+  }, [id]);
   return (
     <div className={styles.projectContainer}>
-      <p>{project.id}</p>
-      <p>{project.type}</p>
-      <p>{project.name}</p>
-      <p>{project.description}</p>
+      <p>{project?.id}</p>
+      <p>{project?.type}</p>
+      <p>{project?.name}</p>
+      <p>{project?.description}</p>
     </div>
   );
 };
--- src/utils/images.jsx
+++
@@ -1,6 +0,0 @@
-import projects from "./projects";
-let sd ={};
-projects.forEach( proj =>{
-  sd.[proj.name] = import i from '/images'
-
-})
