a54b25bb3540449b652fc2fab4b77a17574c3f9c
Alex G. Mircean <56928436+bigmirc@users.noreply.github.com>
2022-10-06 23:31:39 +0300

    muecosminacho (#12)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,33 @@
-import styles from '../styles/Home.module.css'
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import useIsMobile from '../hooks/useIsMobile';
 
-export default function Home() {
+export function Home() {
+  const { isMobile } = useIsMobile();
   return (
     <>
-    <div className={styles.container}>
+      <Head>
+        <title>Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className={isMobile ? styles.containerMobile : styles.container}>
         <div className={styles.andrei}>Andrei</div>
-        <div className={styles.cosmin}>Cosmin</div>
-        <div className={styles.bala}>Bălă</div>
-        <div className={styles.graphic}>Graphic</div>
-        <div className={styles.designer}>Designer</div>
-    </div>
+        <div className={isMobile ? styles.cosminMobile : styles.cosmin}>
+          Cosmin
+        </div>
+        <div className={isMobile ? styles.balaMobile : styles.bala}>Bălă</div>
+        <div className={isMobile ? styles.graphicMobile : styles.graphic}>
+          Graphic
+        </div>
+        <div className={isMobile ? styles.designerMobile : styles.designer}>
+          Designer
+        </div>
+      </div>
     </>
   );
 }
+
+export default dynamic(() => Promise.resolve(Home), {
+  ssr: false,
+});
