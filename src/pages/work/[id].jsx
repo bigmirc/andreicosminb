e83a88cd541d862606e32bc9035bae0ab6f878fd
Alexandru Mircean <amircean@yahoo.com>
2022-10-07 14:23:59 +0300

    updates

--- src/pages/work/[id].jsx
+++ src/pages/work/[id].jsx
@@ -1,6 +1,6 @@
+import Head from 'next/head';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-
 import React, { useEffect, useState } from 'react';
 import projects from '../../utils/projects';
 import styles from './work.module.css';
@@ -17,35 +17,40 @@ const WorkDetail = () => {
     setProject(projects[+id]);
   }, [id]);
   return (
-    <div className={styles.projectContainer}>
-      <Image
-        src={`/images/${project.name}/cover.jpg`}
-        layout="responsive"
-        alt={project.name}
-        width={1740}
-        height={980}
-      ></Image>
-      <div className={styles.projectDetailContainer}>
-        <div className={styles.top}>
-          <p className={styles.projectName}>{project?.name}</p>
-          <p>{project?.description}</p>
-        </div>
-        <div className={styles.bottom}>
-          <div className={styles.detailBox}>
-            <p className={styles.detailBoxTitle}>Area</p>
-            <p>{project?.type}</p>
+    <>
+      <Head>
+        <title>Work</title>
+      </Head>
+      <div className={styles.projectContainer}>
+        <Image
+          src={`/images/${project.name}/cover.jpg`}
+          layout="responsive"
+          alt={project.name}
+          width={1740}
+          height={980}
+        ></Image>
+        <div className={styles.projectDetailContainer}>
+          <div className={styles.top}>
+            <p className={styles.projectName}>{project?.name}</p>
+            <p>{project?.description}</p>
           </div>
-          <div className={styles.detailBox}>
-            <p className={styles.detailBoxTitle}>Year of production</p>
-            <p>{project?.year}</p>
-          </div>
-          <div className={styles.detailBox}>
-            <p className={styles.detailBoxTitle}>Kind</p>
-            <p>{project?.kind}</p>
+          <div className={styles.bottom}>
+            <div className={styles.detailBox}>
+              <p className={styles.detailBoxTitle}>Area</p>
+              <p>{project?.type}</p>
+            </div>
+            <div className={styles.detailBox}>
+              <p className={styles.detailBoxTitle}>Year of production</p>
+              <p>{project?.year}</p>
+            </div>
+            <div className={styles.detailBox}>
+              <p className={styles.detailBoxTitle}>Kind</p>
+              <p>{project?.kind}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
